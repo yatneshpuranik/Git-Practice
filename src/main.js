86a9a8ab8c38da4d6654549cc744cdd05844d389
yatneshpuranik <yatneshpuranik@gmail.com>
2025-03-27 16:45:59 +0530

    Your commit message

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ document.querySelector('#app').innerHTML = `
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn 
+      sadkjfhsdkhfds
     </p>
   </div>
 `
